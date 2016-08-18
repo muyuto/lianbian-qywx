80345325e6134c468fff79ccd76e9dc6b2a23b4a
Akins.Chow <muyuto@163.com>
2016-08-18 14:54:58 +0800

    学习如何创建yii2框架的extension

---
+++ Lbqywx.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouming
+ * Date: 16/8/17
+ * Time: 15:53
+ */
+
+namespace lianbian\yii2\qywx;
+
+use Yii;
+use yii\base\Exception;
+use yii\helpers\Json;
+
+class Lbqywx
+{
+    /**
+     * Returns the fully qualified name of this class.
+     * @return string the fully qualified name of this class.
+     */
+    public static function className()
+    {
+        return get_called_class();
+    }
+}
